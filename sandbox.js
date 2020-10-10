c9025cb316e0ce42379511b54458959d95398d83
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-10 20:49:45 +0200

    Provided functionality for when user submits a grocery and if he tries to add an empty value

--- sandbox.js
+++ sandbox.js
@@ -6,6 +6,9 @@ window.onload = function(){
   var input = form.querySelector("input[type='text']");
   var submit = form.querySelector("input[type='submit']");
   var clearAll = document.querySelector(".clear-all button");
+  var ul = document.querySelector("ul");
+
+  // submit.innerHTML = "<i class='fas fa-pen-square'></i>";
 
   var time = new Date();
   var date = document.createElement("p");
@@ -33,6 +36,38 @@ window.onload = function(){
       container.insertBefore(date, form);
   }
 
+  function addItem(a){
+    ul.innerHTML += `<li>${a}<span><i class="fas fa-trash delete"></i></span></li>`;
+  }
 
+  // Add event listener to submit a grocery on the list
+  form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    var value = form.groceryItem.value.trim();
+
+    if(value.length >= 1){
+      addItem(value);
+      var inserted = document.createElement("p");
+      inserted.id = "inserted";
+      inserted.textContent = "Grocery added to the list";
+      container.insertBefore(inserted, form);
+      setTimeout(function(){
+        container.removeChild(inserted);
+      }, 4000);
+      form.reset();
+    } else{
+      var alert = document.createElement("p");
+      alert.id = "warning";
+      alert.textContent = "You have to add something!!!";
+      container.insertBefore(alert, form);
+      setTimeout(function(){
+        container.removeChild(alert);
+      }, 4000);
+    }
+
+
+  });
 
 }
